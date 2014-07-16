440a0b089f1f3f7e09cd9d97e48a0bf1d499abac
Plahn <e302875@isgm9529dkq4.acct04.us.lmco.com>
2014-07-16 16:20:01 -0400

    Onto IO

---
+++ IO/shortLinesOnly.hs
@@ -0,0 +1,12 @@
+main = interact shortLinesOnly
+
+
+shortLinesOnly :: String -> String
+shortLinesOnly input = 
+	let allLines = lines input
+	shortLines = filter (\line -> length line < 10) allLines
+	result = unlines shortLines 
+	in result
+
+
+-- main = interact $ unlines . filter ((<10) . length) . lines
